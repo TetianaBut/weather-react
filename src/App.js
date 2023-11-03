757effbd61d05de583b874d5b2570a744893cfed
TetianaBut <tbutok54@gmail.com>
2023-11-03 11:28:48 +0100

    Added axios intgration so it shows the weather of a city

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-center">Hello Weather React </h1>
+        <Weather />
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+export default function Weather() {
+  let units = "metric";
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState({});
+  // let [loaded, setLoaded] = useState(false);
+
+  function displayWeather(response) {
+    // setLoaded(true);
+    // alert(`The weather in ${city} is ${response.data.weather[0].description}`);
+    setWeather({
+      name: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+      country: response.data.sys.country,
+    });
+  }
+
+  function retrieveDataWeather(cityName) {
+    let apiKey = "36eeef5b0cb8b4f4de85392d5b87261c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?${cityName}&appid=${apiKey}&units=${units}`;
+    // alert(apiUrl);
+    axios.get(apiUrl).then(displayWeather);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    retrieveDataWeather(`q=${city}`);
+    // let cityName = `q=${city}`;
+    // let apiKey = "36eeef5b0cb8b4f4de85392d5b87261c";
+    // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?${cityName}&appid=${apiKey}&units=${units}`;
+    // alert(apiUrl);
+    // axios.get(apiUrl).then(displayWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  // function handleCurrentCity(event) {
+  //   // updateCityStart();
+  //   event.preventDefault();
+  //   alert(`The weather in ${city} }`);
+  //   retrieveDataWeather(`q=${city}`);
+  // }
+  function updateCityStart(event) {
+    function handleCurrentCity() {
+      // updateCityStart();
+      //  event.preventDefault();
+      // alert(`The weather in ${city} }`);
+      retrieveDataWeather(`q=${city}`);
+    }
+    event.preventDefault();
+    setCity("kyiv");
+    // alert(`The weather in ${city} }`);
+    handleCurrentCity();
+
+    // handleSubmit();
+  }
+
+  return (
+    <div className="Weather-container">
+      <form className="form-input}">
+        <div className="row d-flex justify-content-center">
+          <div className="col-5">
+            <input
+              type="search"
+              placeholder="Input name city.."
+              className="form-control search-input"
+              autoFocus="on"
+              required
+              onChange={updateCity}
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              className="btn btn-primary w-100 location-btn"
+              value="Search"
+              onClick={handleSubmit}
+            />
+          </div>
+          <div className="col-4">
+            <button
+              type="button"
+              className="btn btn-primary w-100 location-btn"
+              onClick={updateCityStart}
+            >
+              Current location
+            </button>
+          </div>
+        </div>
+      </form>
+      <div>
+        <div className="country">
+          <h1>{weather.name},</h1>
+          <em>{weather.country}</em>
+        </div>
+        <ul>
+          <li>tuesday 10:00</li>
+          <li>{weather.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-7 d-flex Weather-temperature">
+          <img src={weather.icon} alt={weather.description} />
+          <strong>{Math.round(weather.temperature)}</strong>
+          <a href="/">°C</a>
+        </div>
+        <div className="col-5">
+          <ul>
+            <li>Humidity: {weather.humidity}%</li>
+            <li>Wind: {weather.wind}km/h</li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
