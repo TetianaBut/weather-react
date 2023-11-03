35c1e28308c8a76fe1efe3cd6e8b3597e8c5b831
TetianaBut <tbutok54@gmail.com>
2023-11-03 12:53:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,17 +16,23 @@ function App() {
         <a
           href="https://www.shecodes.io/graduates/93245-tetiana-butok"
           target="_blank"
+          rel="noreferrer"
         >
           Tetiana Butok
         </a>{" "}
         and open-
-        <a href="https://github.com/TetianaBut/weather-react" target="_blank">
+        <a
+          href="https://github.com/TetianaBut/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
           sourse code on GitHub
         </a>
         {" / "}
         <a
           href="https://mellifluous-hotteok-aa7314.netlify.app/"
           target="_blank"
+          rel="noreferrer"
         >
           hosted on Netlify
         </a>
