0b0a7f65bedfb7fb9e1e1c8ff4151cc5a2d194ae
TetianaBut <tbutok54@gmail.com>
2023-11-03 12:34:08 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -8,8 +8,29 @@ function App() {
     <div className="App ">
       <header className="App-header">
         <h1 className="text-center">Hello Weather React </h1>
+
         <Weather />
       </header>
+      <footer className="text-center">
+        This page was built by 👩‍💻
+        <a
+          href="https://www.shecodes.io/graduates/93245-tetiana-butok"
+          target="_blank"
+        >
+          Tetiana Butok
+        </a>{" "}
+        and open-
+        <a href="https://github.com/TetianaBut/weather-react" target="_blank">
+          sourse code on GitHub
+        </a>
+        {" / "}
+        <a
+          href="https://mellifluous-hotteok-aa7314.netlify.app/"
+          target="_blank"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
