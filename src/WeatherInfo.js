35cf2065470851f0ad9879081423c4b940adf297
TetianaBut <tbutok54@gmail.com>
2023-11-07 18:38:08 +0100

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -26,7 +26,7 @@ export default function WeatherInfo(props) {
         <div className="col-5">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {Math.round(props.data.temperature * 3.6)}km/h</li>
+            <li>Wind: {Math.round(props.data.wind * 3.6)}km/h</li>
             <li></li>
           </ul>
         </div>
